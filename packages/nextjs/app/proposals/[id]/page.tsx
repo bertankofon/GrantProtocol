4c792983d1459bffc59e2ef2e3d047e90300b4a2
fac3m4n <akerimberdi@gmail.com>
2024-03-29 12:01:20 +0200

    adding mock data

--- packages/nextjs/app/proposals/[id]/page.tsx
+++ packages/nextjs/app/proposals/[id]/page.tsx
@@ -1,36 +1,25 @@
 import React from "react";
-// import Image from "next/image";
-import Link from "next/link";
+import proposalMockData from "../../../mock/proposal-data";
+import { SiGithub, SiX } from "@icons-pack/react-simple-icons";
+import { LinkIcon } from "@heroicons/react/24/outline";
 
-const ProposalDetailsPage = ({ params }: { params: { id: string } }) => {
-  const proposal_mock = {
-    id: params.id,
-    status: "Approved",
-    title: "Project Title",
-    owner: "0x4b7866e717f27Fa1C38313D25F647aE0598571BD",
-    created: new Date().toISOString(),
-    votes: {
-      for: 5,
-      against: 2,
-      abstain: 1,
-    },
-    description: "Project description",
-    image: "https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg",
-    raised: 1000,
-  };
+// import Image from "next/image";
+
+const ProposalDetailsPage = ({ params }: { params: { id: number } }) => {
   return (
-    <div className="flex flex-col space-y-4">
+    <div className="flex flex-col space-y-10">
       <div className="flex justify-between items-center">
         <div>
-          <div className="badge badge-success">{proposal_mock.status}</div>
-          <h1 className="text-2xl">{proposal_mock.title}</h1>
+          <div className="badge badge-success">{proposalMockData[params.id].status}</div>
+          <h1 className="text-2xl">{proposalMockData[params.id].name}</h1>
           <p className="text-gray-500">
             Proposed by{" "}
             <span className="link border-l-pink-50">
-              {proposal_mock.owner.slice(0, 6) + "..." + proposal_mock.owner.slice(-4)}
+              {proposalMockData[params.id].owner.slice(0, 6) + "..." + proposalMockData[params.id].owner.slice(-4)}
             </span>{" "}
-            on {new Date(proposal_mock.created).toISOString().substring(0, 10)}
+            on {new Date(proposalMockData[params.id].created).toISOString().substring(0, 10)}
           </p>
+          <p>Raising: {proposalMockData[params.id].raising} DAI</p>
         </div>
         <button className="btn btn-primary">Submit Vote</button>
       </div>
@@ -40,7 +29,7 @@ const ProposalDetailsPage = ({ params }: { params: { id: string } }) => {
           <div className="card-body">
             <div className="flex flex-row items-center justify-between">
               <h2 className="card-title text-success w-full">For</h2>
-              <p className="card-title">{proposal_mock.votes.for}</p>
+              <p className="card-title">{proposalMockData[params.id].votes.for}</p>
             </div>
             <progress className="progress progress-success w-full" value="60" max="100"></progress>
           </div>
@@ -49,7 +38,7 @@ const ProposalDetailsPage = ({ params }: { params: { id: string } }) => {
           <div className="card-body">
             <div className="flex flex-row items-center justify-between">
               <h2 className="card-title text-error w-full">Against</h2>
-              <p className="card-title">{proposal_mock.votes.against}</p>
+              <p className="card-title">{proposalMockData[params.id].votes.against}</p>
             </div>
             <progress className="progress progress-error w-full" value="30" max="100"></progress>
           </div>
@@ -58,34 +47,42 @@ const ProposalDetailsPage = ({ params }: { params: { id: string } }) => {
           <div className="card-body">
             <div className="flex flex-row items-center justify-between">
               <h2 className="card-title w-full">Abstain</h2>
-              <p className="card-title">{proposal_mock.votes.abstain}</p>
+              <p className="card-title">{proposalMockData[params.id].votes.abstain}</p>
             </div>
             <progress className="progress w-full" value="10" max="100"></progress>
           </div>
         </div>
       </div>
 
-      <div className="mb-4">
-        {/* <Image src={proposal_mock.image} alt={proposal_mock.title} width={1000} height={1000} /> */}
-        <p className="text-lg">{proposal_mock.title}</p>
-        <p className="text-gray-500">{proposal_mock.description}</p>
-        <div className="flex justify-between items-center">
-          <p>Raised: {proposal_mock.raised} ETH</p>
-          <Link href={`/proposals/${params.id}/contribute`} className="btn btn-primary">
-            Contribute
-          </Link>
+      <div role="tablist" className="tabs tabs-bordered">
+        <input type="radio" name="my_tabs_1" role="tab" className="tab text-lg" aria-label="About" defaultChecked />
+        <div role="tabpanel" className="tab-content p-10">
+          <div className="grid grid-cols-3 gap-4">
+            <div className="flex items-center space-x-2">
+              <LinkIcon className="h-6 w-6" />
+              <p>{proposalMockData[params.id].socials.website}</p>
+            </div>
+            <div className="flex items-center space-x-2">
+              <SiX size={24} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} />
+              <p>{proposalMockData[params.id].socials.twitter}</p>
+            </div>
+            <div className="flex items-center space-x-2">
+              <SiGithub size={24} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} />
+              <p>{proposalMockData[params.id].socials.github}</p>
+            </div>
+          </div>
+          <div dangerouslySetInnerHTML={{ __html: proposalMockData[params.id].description }} />
         </div>
-      </div>
-      <div className="mb-4">
-        <p>Owner: {proposal_mock.owner}</p>
-      </div>
-      <div className="mb-4">
-        <p>Status: {proposal_mock.status}</p>
-        <p>Votes: 0</p>
-        <p>Comments: 0</p>
-        <Link href={`/proposals/${params.id}/comments`} className="btn btn-primary">
-          View Comments
-        </Link>
+
+        <input type="radio" name="my_tabs_1" role="tab" className="tab text-lg" aria-label="Stats" />
+        <div role="tabpanel" className="tab-content p-10">
+          Tab content 2
+        </div>
+
+        <input type="radio" name="my_tabs_1" role="tab" className="tab text-lg" aria-label="Rounds" />
+        <div role="tabpanel" className="tab-content p-10">
+          Tab content 3
+        </div>
       </div>
     </div>
   );

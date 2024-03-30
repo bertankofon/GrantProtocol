22d02da2a636a88d321d0cf4dfd9f4bf1e3a0048
fac3m4n <akerimberdi@gmail.com>
2024-03-30 06:05:38 +0200

    pool page

--- packages/nextjs/app/pools/page.tsx
+++ packages/nextjs/app/pools/page.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Link from "next/link";
+import pools from "../../mock/pool-data";
 
 const PoolsPage = () => {
-  return <div>PoolsPage</div>;
+  return (
+    <div className="grid grid-cols-3 gap-8">
+      {pools.map(pool => {
+        return (
+          <Link key={pool.id} href={`/pools/${pool.id}`} className="card bg-base-100 shadow-xl">
+            <div className="card-body">
+              <div className="flex justify-between">
+                <h2 className="card-title">{pool.name}</h2>
+                <h2 className="card-title">DAI {pool.tvl}</h2>
+              </div>
+              <p>{pool.description}</p>
+            </div>
+          </Link>
+        );
+      })}
+    </div>
+  );
 };
 
 export default PoolsPage;
---
+++ packages/nextjs/mock/pool-data.ts
@@ -0,0 +1,25 @@
+const pools = [
+  {
+    id: 1,
+    name: "ZK",
+    description: "Let ZK roll!",
+    token: "DAI",
+    tvl: "500,000",
+  },
+  {
+    id: 2,
+    name: "GameFi",
+    description: "Pool dedicated for web3 games",
+    token: "DAI",
+    tvl: "300,000",
+  },
+  {
+    id: 3,
+    name: "DeFi",
+    description: "Pool for DeFi projects",
+    token: "DAI",
+    tvl: "1,000,000",
+  },
+];
+
+export default pools;
